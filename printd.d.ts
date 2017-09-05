4e830bd81320502d3e7cf9da699e826473e47aa3
José Luis Quintana <joseluisquintana20@gmail.com>
2017-09-05 18:06:22 +0200

    fix types

--- printd.d.ts
+++ printd.d.ts
@@ -1,8 +1,8 @@
 export class Printd {
   constructor (parent?: HTMLElement)
-
-  print (el: HTMLElement, cssText?): void
+  print (el: HTMLElement, cssText?: string): void
 }
 
 export function createIFrame (parent?: HTMLElement): HTMLIFrameElement
 export function createStyle (doc: Document, cssText: string): HTMLStyleElement
+
