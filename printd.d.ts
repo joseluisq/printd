9e7f6e2785f998abb07751e19ffe887b22a6316a
José Luis Quintana <joseluisquintana20@gmail.com>
2017-09-05 17:48:35 +0200

    update types

--- printd.d.ts
+++ printd.d.ts
@@ -1,8 +1,8 @@
 export class Printd {
-  constructor (parent: HTMLElement)
+  constructor (parent?: HTMLElement)
 
-  print (el: HTMLElement, cssText: string): void
+  print (el: HTMLElement, cssText?): void
 }
 
+export function createIFrame (parent?: HTMLElement): HTMLIFrameElement
 export function createStyle (doc: Document, cssText: string): HTMLStyleElement
-export function createIFrame (parent: HTMLElement): HTMLIFrameElement
